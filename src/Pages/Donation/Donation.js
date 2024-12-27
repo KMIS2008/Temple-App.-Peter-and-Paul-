d0eff7d975c5e90bf1ec82dcefc66a3ca6b6fe2f
KMIS2008 <lion.irisha@gmail.com>
2024-12-27 17:41:55 +0200

    create component donation

--- src/Pages/Donation/Donation.js
+++ src/Pages/Donation/Donation.js
@@ -1,6 +1,11 @@
+import {Donations} from '../../components/Donation/Donation';
+import { Tittle } from 'components/Title/Title';
+
 export default function Donation(){
     return(
         <>
+        <Tittle title="Добровільна пожертва"/>
+        <Donations/>
         </>
     )
 }
---
+++ src/components/Donation/Donation.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Text=styled.p`
+text-align: justify;
+line-height: 1.5;
+
+`
