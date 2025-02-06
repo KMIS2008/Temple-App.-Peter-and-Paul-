95f0ba0a4c64a6f1dffc9a8431841f1174706029
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2025-02-06 10:20:53 +0200

    correct style ShareLayout

--- src/components/SharedLayout/SharedLayout.styled.js
+++ src/components/SharedLayout/SharedLayout.styled.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 export const Contater = styled.div`
 width: auto;
-min-width: 320px;
-max-width: 1440px;
+/* min-width: 320px;
+max-width: 1440px; */
 margin: 0 auto;
 background: ${p=>p.theme.colors.whiteblue};
 `
