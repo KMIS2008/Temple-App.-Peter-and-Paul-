c6082fad5df8210ac4b32518e15f2f984dc751c1
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2025-02-06 12:18:56 +0200

    create Pages Confession

--- src/Pages/Confession/Confession.js
+++ src/Pages/Confession/Confession.js
@@ -1,6 +1,12 @@
+import {ConfessionDescription} from '../../components/ConfessionDescription/ConfessionDescription';
+import {Container} from './Confession.styled';
+import { Tittle } from "components/Title/Title";
+
 export default function Confession(){
     return(
-        <>
-        </>
+        <Container>
+             <Tittle title={"Сповідь"}/>
+             <ConfessionDescription/>
+        </Container>
     )
 }
