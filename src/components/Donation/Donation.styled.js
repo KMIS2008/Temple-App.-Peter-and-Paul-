5c4e1624d70bb8b679b85c568c1d5c70a2b45747
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2025-02-07 13:57:12 +0200

    update Donation

--- src/components/Donation/Donation.styled.js
+++ src/components/Donation/Donation.styled.js
@@ -3,6 +3,14 @@ import styled from "styled-components";
 export const Text=styled.p`
 margin: 20px 20px;
 text-align: justify;
+text-indent: 30px;
 line-height: 1.5;
+`
 
+export const Title=styled.p`
+font-style: italic;
+`
+
+export const Span=styled.span`
+font-style: italic;
 `
