abe902cecaac95b9fc2d45da4f8a38e47b6b76a4
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2025-03-21 15:23:48 +0200

    correct text page Wedding

--- src/components/Wedding/Wedding.styled.js
+++ src/components/Wedding/Wedding.styled.js
@@ -1,19 +1,46 @@
 import styled from "styled-components";
 
-export const Text=styled.p`
+export const Container = styled.div`
+padding: 40px;
+`
+
+export const Title = styled.h4`
+margin-bottom: 20px;
+color: #483d8b;
+text-align: center;
+`
+
+export const Text = styled.p`
+margin-bottom: 20px;
 text-indent: 30px;
 text-align: justify;
 `
 
+export const Span = styled.span`
+font-weight:500;
+`
+export const SpanText = styled.span`
+font-style: italic;
+`
+
+export const SpanItalic = styled.span`
+font-weight:500;
+font-style: italic;
+`
+
 export const TitleChapter=styled.h4`
 margin-top: 20px;
-
 text-indent: 30px;
 
 `
 
 export const List= styled.ul`
+padding: 0 40px;
 text-indent: 40px;
 text-align: justify;
 list-style: none;
+`
+
+export const ListNeed = styled.ul`
+padding-left: 80px;
 `
